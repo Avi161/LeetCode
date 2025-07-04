b9ebd25302842f7a99f73820064a44618299310a
Avi161 <paudela@union.edu>
2025-07-04 16:33:05 -0400

    update build tree

--- binary_trees/build_tree.py
+++ binary_trees/build_tree.py
@@ -1,4 +1,4 @@
-# Definition for a binary tree node.
+# Definition for a binary tree node. PREORDER + INORDER
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
