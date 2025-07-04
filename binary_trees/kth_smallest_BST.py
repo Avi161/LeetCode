565ac15a9910350e0d06dd04188642364464042e
Avi161 <paudela@union.edu>
2025-07-04 12:44:40 -0400

    Add kth smallest

---
+++ binary_trees/kth_smallest_BST.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        count = 0
+        cur = root
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            node = stack.pop()
+            count += 1
+            if count == k:
+                return node.val
+
+            cur = node.right
+
+        #Recursive
+        # arr = []
+
+        # def dfs(node):
+        #     if not node:
+        #         return
+            
+        #     dfs(node.left)
+        #     arr.append(node.val)
+        #     dfs(node.right)
+        
+        # dfs(root)
+        # return arr[k - 1]
